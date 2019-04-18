0f420e94f875c3915b79713de2921d490ad92d6c
Niloofar Shahbaz <niloofar1376@gmail.com>
2019-04-18 19:21:24 +0430

    chat e ye vari :)))

--- client.py
+++ client.py
@@ -25,31 +25,22 @@ class Client(Thread):
         self.connected = False
 
     def send(self, receiving_client_address):
-        file_path = raw_input(str(self.my_port) + " : input file:\n")
-        client_answer = self.request_to_send(receiving_client_address, file_path)
+        client_answer = self.request_to_send(receiving_client_address)
         if client_answer is None:
             return
         if client_answer == 'accept':
-            self.send_audio(file_path)
+            self.send_audio()
 
     def receive(self):
-        audio_name, audio_format, sample_width, channels, rate = self.get_request()
+        if self.get_request():
+            self.receive_audio()
 
-        if audio_name:
-            self.receive_audio(audio_name, audio_format, sample_width, channels, rate)
-
-    def send_audio(self, file_path):
-        # audio = open(file_path, 'rb')
-        # data = audio.read(buf_size)
-        # while data:
-        #     if self.udp_soc.sendto(data, (self.server_host, self.server_port)):
-        #         data = audio.read(buf_size)
-        #         time.sleep(0.01)
+    def send_audio(self):
         CHUNK = 1024
         FORMAT = pyaudio.paInt16
         CHANNELS = 1
         RATE = 44100
-        RECORD_SECONDS = 3
+        RECORD_SECONDS = 4
         WAVE_OUTPUT_FILENAME = "output.wav"
         p = pyaudio.PyAudio()
 
@@ -66,9 +57,6 @@ class Client(Thread):
             frames.append(data)
             self.udp_soc.sendto(data, (self.server_host, self.server_port))
 
-        # while True:
-
-
         print("* done recording")
 
         stream.stop_stream()
@@ -82,15 +70,13 @@ class Client(Thread):
         wf.writeframes(b''.join(frames))
         wf.close()
 
-    def receive_audio(self, audio_name, audio_format, sample_width, channels, rate):
-        # audio = open(audio_name + '1.' + audio_format, 'wb')
+    def receive_audio(self):
         WAVE_OUTPUT_FILENAME = "output_final.wav"
         frames=[]
         CHUNK = 1024
         FORMAT = pyaudio.paInt16
         CHANNELS = 1
-        RATE = 44100
-        RECORD_SECONDS = 5
+        RATE = 24100
 
         p = pyaudio.PyAudio()
         stream = p.open(format=FORMAT,
@@ -102,16 +88,11 @@ class Client(Thread):
 
         try:
             while data:
-                # audio.write(data)
                 frames.append(data)
-                stream.write(data)
                 self.udp_soc.settimeout(1)
                 data, address = self.udp_soc.recvfrom(buf_size)
         except socket.timeout:
             print('bye')
-            stream.stop_stream()
-            stream.close()
-            p.terminate()
             self.udp_soc.close()
             wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
             wf.setnchannels(CHANNELS)
@@ -119,30 +100,18 @@ class Client(Thread):
             wf.setframerate(RATE)
             wf.writeframes(b''.join(frames))
             wf.close()
-            # audio.close()
+            for frame in frames:
+                stream.write(frame)
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
 
-    def request_to_send(self, receiving_client_address, file_path):
+    def request_to_send(self, receiving_client_address):
 
         request = 'RequestToSend'
-        audio = open(file_path, 'rb')
-        audio_name = path.basename(audio.name)
-        audio_size = path.getsize(audio.name)
-        audio_name, audio_format = path.splitext(audio_name)
-        audio_format = audio_format[1:]
-        audio.close()
-        audio = wave.open(file_path, 'rb')
-        sample_width = audio.getsampwidth()
-        channels = audio.getnchannels()
-        frame_rate = audio.getframerate()
 
         msg = {'request': request,
                'to': receiving_client_address,
-               'audio_name': audio_name,
-               'audio_size': audio_size,
-               'audio_format': audio_format,
-               'pyaudio_sample_width': sample_width,
-               'pyaudio_channels': channels,
-               'pyaudio_framerate': frame_rate
                }
         packet = json.dumps(msg).encode('utf-8')
         length = struct.pack('!I', len(packet))
@@ -182,11 +151,6 @@ class Client(Thread):
         if msg.get('request') == 'RequestToSend':
             choice = raw_input(str(self.my_port) + " : do you accept this audio?[y/n]\n")
             if choice == 'y':
-                audio_name = msg.get('audio_name')
-                audio_format = msg.get('audio_format')
-                sample_width = msg.get('pyaudio_sample_width')
-                channels = msg.get('pyaudio_channels')
-                rate = msg.get('pyaudio_framerate')
                 msg = {'request': 'AcceptRequest',
                        'answer': 'accept'
                        }
@@ -195,7 +159,7 @@ class Client(Thread):
                 packet = length + packet
                 self.tcp_soc.sendall(packet)
 
-                return audio_name, audio_format, sample_width, channels, rate
+                return True
             else:
                 self.connected = False
                 return
